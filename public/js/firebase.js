389f961e87b28fad8c70a4cbafef68c634daf212
chanpreet1999 <chanpreet1999@gmail,com>
2022-01-15 16:05:54 +0530

    Update : Major functionality done

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,6 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfrIrsuwjGetsxRPSJ9IhFT0lCXsr1MbI",
-  authDomain: "blogging-website-72a8f.firebaseapp.com",
-  projectId: "blogging-website-72a8f",
-  storageBucket: "blogging-website-72a8f.appspot.com",
-  messagingSenderId: "982431297298",
-  appId: "1:982431297298:web:14f295dd89aebd73b64dcb"
+  // your firebase config
 };
 
 firebase.initializeApp(firebaseConfig);
